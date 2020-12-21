3b9b8c28d0374ffd2b13d06f1af9a03ee0fc50c8
CEO82 <a.lozhin@gmail.com>
2020-12-21 15:30:02 +0300

    L3 HW2 is Finished

--- HW Lesson 3/L3 HW2.py
+++ HW Lesson 3/L3 HW2.py
@@ -3,17 +3,30 @@
 # должна принимать параметры как именованные аргументы. Реализовать вывод данных о
 # пользователе одной строкой.
 
+"""Данная функция для проверки число ли введенный символ?"""
+def isfloat(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+"""Упаковка введенных данных в словарь и проверка года и тел №"""
 def user_info_input(**kwargs):
+    if isfloat(kwargs['birthYear']) == False or isfloat(kwargs['phone']) == False or len(kwargs['phone']) != 11 or len(kwargs['birthYear']) != 4:
+        print('В полях год рождения и № телефона вы ввели не верные данные! \nПожалуйста повторите ввод, год содержит 4 цыфры, № телефона 11 цыфр')
+        check = False
+        return check
     print(kwargs)
 
-usrName = 'Введите ваше имя: '
-usrSurName = 'Введите вашу фамилию: '
-usrBirthYear = 'Введите год вашего рождения: '
-usrSity = 'Введите город вашего проживания: '
-usrEmail = 'Введите вашу элю почту: '
-usrPhone = 'Введите ваш № телефон, без знака + и скобок в формате 89********: '
 
-questUserInfo = []
+questUserInfo = ['Введите ваше имя: ', 'Введите вашу фамилию: ', 'Введите год вашего рождения: ', 'Введите город вашего проживания: ', 'Введите вашу элю почту: ', 'Введите ваш № телефон, без знака + и скобок в формате 89********: ']
 
-
-user_info_input(name = input(usrName), surName = input(usrSurName), birthYear = input(usrBirthYear), sity = input(usrSity), email = input(usrEmail), phone = input(usrPhone))
+while True:
+    a = user_info_input(name=input(usrName), surName=input(usrSurName), birthYear=input(usrBirthYear), sity=input(usrSity), email=input(usrEmail), phone=input(usrPhone))
+    if a == False:
+        print('Повторный ввод')
+        a = user_info_input(name=input(usrName), surName=input(usrSurName), birthYear=input(usrBirthYear), sity=input(usrSity), email=input(usrEmail), phone=input(usrPhone))
+    else:
+        break
+print('Ввод данных успешно завершен!')
