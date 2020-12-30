f4312ce59bcfbf5453eaa517dd53731998e16de3
CEO82 <a.lozhin@gmail.com>
2020-12-30 23:13:20 +0300

    HW 4 Finished, HW 5 added

--- HW Lesson 5/L3 HW 4.py
+++ HW Lesson 5/L3 HW 4.py
@@ -8,3 +8,28 @@
 # записываться в новый текстовый файл.
 
 
+numbDict = {
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+}
+
+with open('English_numb_in2.txt', 'r', encoding='utf-8') as usrFile:
+    usrList = usrFile.readlines()
+    count = 0
+    for i in usrList:
+        strKonv = i.rstrip('\n')
+        listKonv = strKonv.split()
+        translate = numbDict[listKonv[0].lower()]
+        listKonv.pop(0)
+        listKonv.insert(0, translate.title())
+        strKonv = ' '.join(listKonv)
+        if count > 0:
+            ind = 'a'
+        else:
+            ind = 'w'
+
+        with open('English_numb_out2.txt', ind, encoding='utf-8') as usrFileRu:
+            print(strKonv, file=usrFileRu)
+        count = 2
