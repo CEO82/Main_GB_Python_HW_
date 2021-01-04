f28a5299cad9f4d2b5a9e1d0a41270d0bc73bc4f
CEO82 <a.lozhin@gmail.com>
2021-01-04 21:19:49 +0300

    HW 4 pre finish

--- HW Lesson 6_2/L6 HW 4.py
+++ HW Lesson 6_2/L6 HW 4.py
@@ -5,7 +5,6 @@
 # ● опишите несколько дочерних классов: TownCar , SportCar , WorkCar , PoliceCar ;
 # ● добавьте в базовый класс метод show_speed, который должен показывать текущую
 # скорость автомобиля;
-# © geekbrains.ru 19
 # ● для классов TownCar и WorkCar переопределите метод show_speed. При значении
 # скорости свыше 60 ( TownCar) и 40 ( WorkCar) должно выводиться сообщение о
 # превышении скорости.
@@ -14,3 +13,116 @@
 
 
 
+class Car:
+    # атрибуты класса:
+
+
+    # Конструктор с атрибутами объекта:
+    def __init__(self, speed , color, model, is_police):
+        self.spd = speed
+        self.col = color
+        self.md = model
+        self.plc = is_police
+
+
+    # методы класса:
+    def show_speed(self):
+        print(f'Ваша скорость {self.spd} км/ч')
+
+    def go(self):
+        if self.plc == False:
+            print(f'Автомобиль {self.md} в движении')
+        else:
+            print(f'Автомобиль {self.md} в движении! Спец сигналы включены!')
+
+    def stop(self):
+        if self.plc == False:
+            print(f'Автомобиль {self.md} в остановился')
+        else:
+            print(f'Автомобиль {self.md} в остановился! Спец сигналы выключены! Погоня завершена!')
+
+    def turn(self):
+        print('Через 300 метров перверните на право')
+
+
+# Объект
+
+class TownCar(Car):
+
+    # атрибуты класса:
+
+    # Конструктор с атрибутами объекта:
+
+    # методы класса:
+    def show_speed(self):
+        if float(self.spd) > 60:
+            print(f'Вы превысили скрость на {float(self.spd) - 60} км/ч! Снизте скорости до 60 км/ч!')
+        else:
+            print(f'Ваша скорость {self.spd} км/ч')
+
+
+# Объект
+
+class SportCar(Car):
+    pass
+
+# атрибуты класса:
+
+# Конструктор с атрибутами объекта:
+
+# методы класса:
+
+# Объект
+
+class WorkCar(Car):
+    # pass
+
+# атрибуты класса:
+
+# Конструктор с атрибутами объекта:
+
+# методы класса:
+    def show_speed(self):
+        if float(self.spd) > 40:
+            print(f'Вы превысили скрость на {float(self.spd) - 40} км/ч! Снизте скорости до 40 км/ч!')
+        else:
+            print(f'Ваша скорость {self.spd} км/ч')
+
+# Объект
+
+class PoliceCar(Car):
+    pass
+
+# атрибуты класса:
+
+# Конструктор с атрибутами объекта:
+
+# методы класса:
+
+# Объект
+dodgeViper = SportCar(280, 'Black', 'Viper', False)
+dodgeViper.go()
+dodgeViper.show_speed()
+dodgeViper.turn()
+dodgeViper.stop()
+print()
+solaris = TownCar(100, 'Silver', 'Solaris', False)
+solaris.go()
+solaris.show_speed()
+solaris.turn()
+solaris.stop()
+print()
+
+vwTransporter = WorkCar(60, 'White', 'Transporter_T_6', False)
+vwTransporter.go()
+vwTransporter.show_speed()
+vwTransporter.turn()
+vwTransporter.stop()
+print()
+
+crownVictoria = PoliceCar(300, 'Black_and_White', 'CrownVictoria', True)
+crownVictoria.go()
+crownVictoria.show_speed()
+crownVictoria.turn()
+crownVictoria.stop()
+print()
