20411f6eb240ea714ce7bdff9d0a9110cf22e7f0
CEO82 <a.lozhin@gmail.com>
2021-01-18 13:33:39 +0300

    HW 2 is finished, HW 3 added

--- HW Lesson 8/L8 HW 2.py
+++ HW Lesson 8/L8 HW 2.py
@@ -4,4 +4,30 @@
 #   При вводе нуля в качестве  делителя программа должна корректно
 #обработать эту ситуацию и не завершиться с ошибкой.
 
+def division(x, y):
+    res = x/y
+    return res
+
+class MyExeptions(Exception):
+
+    # атрибуты класса:
+    # Конструктор с атрибутами объекта:
+    def __init__(self, txt):
+        self.txt = txt
+    # методы класса:
+
+# Объект класса:
+
+print('Attention operation Divsion!')
+
+try:
+    x = float(input('Enter number: '))
+    y = float(input('Enter divisior argument: '))
+    if y == 0:
+        raise MyExeptions('Forbidden to use 0 as divisior argument')
+    res = division(x, y)
+    print(res)
+except (ValueError, MyExeptions, ZeroDivisionError, NameError) as error:
+    print(error)
+
 
